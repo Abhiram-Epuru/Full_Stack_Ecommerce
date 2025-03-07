51619acf6647b0017cc3645ccc3381feb75ab92d
Abhiram <141832900+Abhiram-Epuru@users.noreply.github.com>
2025-03-07 23:54:17 +0530

    Update ShopCategory.jsx

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -8,7 +8,7 @@ const ShopCategory = (props) => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('https://full-stack-ecommerce-backend-3odp.onrender.com') 
+    fetch('https://full-stack-ecommerce-backend-3odp.onrender.com/allproducts') 
       .then((res) => res.json()) 
       .then((data) => setAllProducts(data));
   };
